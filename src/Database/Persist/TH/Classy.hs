76ed09fc8de8b674e81fb07fa8f68a8c45992fc3
MegaBluejay <dmoiseev2011@gmail.com>
2022-10-26 13:08:07 +0300

    fix for new persistent

--- src/Database/Persist/TH/Classy.hs
+++ src/Database/Persist/TH/Classy.hs
@@ -13,6 +13,8 @@ import Data.Char qualified as Char
 import Data.Text qualified as T
 import Data.Traversable (forM)
 import Database.Persist
+import Database.Persist.EntityDef.Internal
+import Database.Persist.Quasi.Internal
 import Language.Haskell.TH.Lib
 import Language.Haskell.TH.Quote
 import Language.Haskell.TH.Syntax
@@ -37,7 +39,7 @@ mkClassyClass name =
         [ ClassD
             []
             (mkName hasName)
-            [PlainTV (mkName "ev"), PlainTV (mkName "a")]
+            [PlainTV (mkName "ev") (), PlainTV (mkName "a") ()]
             [FunDep [mkName "ev"] [mkName "a"]]
             [ SigD
                 (mkName name)
@@ -52,27 +54,27 @@ mkClassyClass name =
 --
 -- instance HasName Person String where
 --   name = (lens personName) (\ x y -> x {personName = y})
-mkClassyInstances :: [EntityDef] -> Q [Dec]
+mkClassyInstances :: [UnboundEntityDef] -> Q [Dec]
 mkClassyInstances defs = do
   concat <$> mapM mkClassyInstance defs
 
-mkClassyInstance :: EntityDef -> Q [Dec]
-mkClassyInstance EntityDef {..} = do
-  forM entityFields $ \FieldDef {..} ->
-    case fieldType of
+mkClassyInstance :: UnboundEntityDef -> Q [Dec]
+mkClassyInstance ued = do
+  forM (unboundEntityFields ued) $ \UnboundFieldDef {..} ->
+    case unboundFieldType of
       FTTypeCon tmodule tname -> do
         let _unused = ()
             -- \| like "Person"
             instanceTypeName =
-              ConT (mkName (T.unpack (unHaskellName entityHaskell)))
+              ConT (mkName (T.unpack (unEntityNameHS $ getUnboundEntityNameHS ued)))
             -- \| like "person"
             instanceTypeNameLowerFirstChar =
-              case T.unpack (unHaskellName entityHaskell) of
+              case T.unpack (unEntityNameHS $ getUnboundEntityNameHS ued) of
                 "" -> ""
                 (x : xs) -> Char.toLower x : xs
             -- \| like "Name"
             fieldUpperFirstChar =
-              case T.unpack (unHaskellName fieldHaskell) of
+              case T.unpack (unFieldNameHS unboundFieldNameHS) of
                 "" -> ""
                 (x : xs) -> Char.toUpper x : xs
             -- \| like "HasName"
@@ -98,14 +100,14 @@ mkClassyInstance EntityDef {..} = do
                 []
             field =
               FunD
-                (mkName (T.unpack (unHaskellName fieldHaskell)))
+                (mkName (T.unpack (unFieldNameHS unboundFieldNameHS)))
                 [fieldClause]
             fieldTName =
               let tnameAndModule = case tmodule of
                     Nothing -> tname
                     Just t -> t <> "." <> tname
                   nonMaybe = ConT (mkName (T.unpack tnameAndModule))
-               in case FieldAttrMaybe `elem` fieldAttrs of
+               in case FieldAttrMaybe `elem` unboundFieldAttrs of
                     False -> nonMaybe
                     True -> AppT (ConT (mkName "Maybe")) nonMaybe
         return $
